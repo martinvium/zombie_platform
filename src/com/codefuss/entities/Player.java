681511bd6aaba73d04d07e37bb326fce526c1b14
Martin Vium <martin.vium@gmail.com>
2011-10-26 21:50:53 +0200

    Fix shooting backwards

--- src/com/codefuss/entities/Player.java
+++ src/com/codefuss/entities/Player.java
@@ -9,6 +9,8 @@ import org.newdawn.slick.geom.Vector2f;
  */
 final public class Player extends Creature {
 
+    final static int AMMO_OFFSET = 17;
+
     public Player(AmmoFactory entityFactory, Vector2f position, Body body) {
         super(entityFactory, position, body);
     }
@@ -17,9 +19,9 @@ final public class Player extends Creature {
     public Entity getMainAttack(int timeKeyPressed) {
         float x;
         if(direction == Direction.LEFT) {
-            x = getPosition().x - 10;
+            x = getPosition().x - AMMO_OFFSET;
         } else {
-            x = getPosition().x + getWidth() + 10;
+            x = getPosition().x + getWidth() + 1;
         }
         
         float y = getPosition().y + getHeight() / 4;
